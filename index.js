162777b67f2afded5644cb923695eb4d0280be7f
Josef Benninghoff <jsbenning4@gmail.com>
2018-11-11 21:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setThisWithApply(fn, thisValue, args){
   return fn.apply(thisValue, args);
 }
 function returnNewFunctionOf(functionToBeCopied, thisValue){
-  return typeof(functionToBeCopied(thisValue));
+  
   
   
 }
